ad7a3789b3965fb02ca818ee52eb8047526e570b
Jonathan W. Smith <81338645+SmithingJon@users.noreply.github.com>
2022-06-19 16:41:35 -0500

    Updated License

--- ProactiveRemediations/Detection - MicrosoftRemoteHelp.ps1
+++ ProactiveRemediations/Detection - MicrosoftRemoteHelp.ps1
@@ -4,6 +4,24 @@
 
         Copyright (c) 2022 SmithingJon Jonathan W. Smith
 
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
     Helper functions taken from OSDCloud.ps1, copyright David Segura, MIT License.
 
         MIT License
@@ -82,7 +100,7 @@ function Write-SectionSuccess {
     Write-Host -ForegroundColor Green $Message
 }
 #endregion
-#region
+#region Title
 Write-DarkGrayLine
 Write-SectionHeader -Message "Detection - Microsoft Remote Help"
 Write-DarkGrayLine
